bb24482eab1e653066796d815924fe107e5afdd6
invertednz <jamesfarrier@gmail.com>
2019-11-16 12:14:14 -0800

    Master Pre Branch Commit 1

---
+++ src/Area2/File1Area2.java
@@ -0,0 +1,9 @@
+package Area2;
+
+/**
+ * Created by james on 13/06/2018.
+ */
+public class File1Area2 {
+	int i=0;
+
+}
---
+++ src/Area3/File1Area3.java
@@ -0,0 +1,9 @@
+package Area3;
+
+/**
+ * Created by james on 13/06/2018.
+ */
+public class File1Area3 {
+	int i=0;
+
+}
